6a51691082c76cd5eda456d27a84d7717ff1126f
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-12-08 12:45:53 +0100

    feat: add a swagger summary to the controller

--- test_task/src/main/java/com/goltsov/test_task/test_task/controller/ControllerExceptionHandler.java
+++ test_task/src/main/java/com/goltsov/test_task/test_task/controller/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.goltsov.test_task.test_task.controller;
 import com.goltsov.test_task.test_task.util.exception.ItemNotFoundException;
 import com.goltsov.test_task.test_task.util.exception.ErrorMessage;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +19,8 @@ import java.util.Date;
 @ResponseBody
 public class ControllerExceptionHandler {
 
+    @Operation(summary = "Process exceptions coming from trying to get an item that does not exit in DB. "
+            + "Form correct and informative exception message")
     @ExceptionHandler(ItemNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorMessage processItemNotFoundException(ItemNotFoundException exception, WebRequest request) {
